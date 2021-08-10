12486b56108e9ea4e939f35116af310e001841df
tanishqvalecha <88627295+tanishqvalecha@users.noreply.github.com>
2021-08-10 16:22:24 +0530

    assignment 1
    Take input for a list and sort it in descending order.

---
+++ project.py
@@ -0,0 +1,12 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nums=[12,52,65,354,85,45,96,32]
+>>> nums.sort()
+>>> nums
+[12, 32, 45, 52, 65, 85, 96, 354]
+>>> nums.reverse()
+>>> nums
+[354, 96, 85, 65, 52, 45, 32, 12]
+>>> print (nums)
+[354, 96, 85, 65, 52, 45, 32, 12]
+>>> 
